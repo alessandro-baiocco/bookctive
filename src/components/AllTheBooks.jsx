388c80bf8aff8db25593310fd680e43b0cb05296
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-09-05 16:22:42 +0200

    better books

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -9,21 +9,22 @@ class AllTheBooks extends Component {
   render() {
     return (
       <>
-        <Container>
+        <Container fluid>
           {this.state.selectedBook ? (
-            <Container>
+            <Container className="my-3 bg-dark text-light">
+              <img src={this.state.selectedBook[2]} alt={this.state.selectedBook[0]} style={{ height: "200px" }} />
               <h3>{this.state.selectedBook[0]}</h3>
               <p>{this.state.selectedBook[1]}</p>
             </Container>
           ) : (
             <></>
           )}
-          <Row className="g-2">
+          <Row className="g-3">
             {libri.map((libro, index) => (
               <Card
                 key={`book-${index}`}
-                className="col-4"
-                onClick={() => this.setState({ selectedBook: [libro.title, libro.price] })}
+                className="col-xl-3 col-md-4 col-6"
+                onClick={() => this.setState({ selectedBook: [libro.title, libro.price, libro.img] })}
               >
                 <img src={libro.img} className="card-img-top" alt={libro.title} />
                 <div className="card-body">
